1008a6a1d53f87730a0827bd47331e872f6c1836
かいと <160225758+j4sperr001@users.noreply.github.com>
2024-05-08 22:42:50 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -1,42 +1,30 @@
-const axios = require('axios');
-
-module.exports.config = {
-  name: "ai",
-  version: "1.0.0",
-  cooldown: 5,
-  role: 0,
-  hasPrefix: false,
-  aliases: [''],
-  description: "this command may help you",
-  usage: "{pref}[name of cmd] [query]",
-  credits: "Google"
-};
-
-module.exports.run = async ({ api, event, args }) => {
-  try {
-    const query = args.join(" ") || "hello";
-    const data = await api.getUserInfo(event.senderID);
-    const { name } = data[event.senderID];
-    
-    const apikey = 'AIzaSyBp9HQ69tdYi2TEywbVNYeDZEln0W8BBf8';
-      api.setMessageReaction("🟡", event.messageID, (err) => console.log(err), true);
-       api.sendMessage(
-        `⚔️ Waiting for response on gemini...`,
-        event.threadID
-      );
-const a = "https://unknown-apis.onrender.com/gemini";
-      axios.get(a, { params: {
-    prompt: query,
-    apikey: apikey
-}}).then((response)=>{
-  console.log(response.data);
-          api.setMessageReaction("🟢", event.messageID, (err) => console.log(err), true);
-        api.sendMessage(response.data.success, event.threadID, event.messageID) 
-          console.log(`Sent Gemini's response to the user`)
-      })
-  } catch (error) {
-    console.error(`🔴 Failed to get Gemini's response: ${error.message}`);
-    const errorMessage = "🔴 An error occurred. You can try typing your query again or resending it. There might be an issue with the server that's causing the problem, and it might resolve on retrying.";
-    api.sendMessage(errorMessage, event.threadID);
-  }
-};
+const {get} = require('axios');
+const url = "https://markdevs-last-api.onrender.com";
+module.exports = {
+    config: {
+       name: "ai",
+       version: "1.0.0",
+       hasPermission: 0,
+       credits: "unknown",
+       description: "OpenAI official AI with no prefix",
+       commandCategory: "education",
+       usePrefix: false,
+       usages: "...",
+       cooldowns: 0
+    },
+    run: async function({api, event, args}){
+            let prompt = args.join(' '), id = event.senderID;
+           async function r(msg){
+                 api.sendMessage(msg, event.threadID, event.messageID)
+             }
+            if(!prompt) return r("Please provide a question first.");
+            r("Please bear with me while I ponder your request...");
+            try {
+                const res = await get(url+"/gpt4?prompt="+prompt+"&uid="+id);
+                const answer = res.data.gpt4;
+                return r(`📦𝗚𝗣𝗧4+ 𝗖𝗢𝗡𝗧𝗜𝗡𝗨𝗘𝗦 𝗔𝗜\n━━━━━━━━━━━━━━━━━━\n𝗤𝘂𝗲𝘀𝘁𝗶𝗼𝗻: ${prompt}\n━━━━━━━━━━━━━━━━━━\n𝗔𝗻𝘀𝘄𝗲𝗿: ${answer}\n\ncredits: www.facebook.com/mark.dev69`);
+            } catch (e){
+                return r(e.message)
+            }
+    }
+}
